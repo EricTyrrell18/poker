e46ffb7d83da607ca74e3ef9e449d1f9d23192c5
Eric Tyrrell <cicero222@Ignoramus.local>
2019-11-09 17:42:42 -0500

    Add basic Game Elements
     - Player updates:
       * get_action() can be called to ask for action
       * Chips handled properly
     - Game updates:
       * Basic CLI texas holdem game
       * Likely Buggy, untested
       * Known Bugs:
         > Doesn't handle ties
         > Doesn't handle side pots

--- src/player.py
+++ src/player.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
 """Module pertaining to Poker Players"""
+
 class Player():
     """Player base class"""
     def __init__(self, name):
         self.name = name
+        self.stack_size = 500
+        #Use this to keep track of how much money they have in the current street of action
+        self.cur_bet = 0
+
+    def __eq__(self, other):
+        return self.name == other.name
 
     def get_hand(self):
         return self.hand
@@ -13,5 +20,81 @@ class Player():
         return self.name
     def set_name(self, name):
         self.name = name
-   
+  
+    def get_action(self, cur_bet):
+        """
+        Determines what actions the players allowed to make and prompts them.
+        Basic scenarios to be aware of include (Big Blind = 2):
+        Noone opens preflop before The bigblind:  check or raise
+        First player to act with 500 chips: Check or bet
+        First player to act with 1 chip: Check or All in
+        First player to act with 3 chips: Check, or Bet
+        
+        For the next few examples theFirst player goes all in for 400 chips,
+        Second Player has 800 chips: Fold, call, All in 
+        Second Player has 300 chips: fold, All in 
+        Second Player has 1800 chips: Fold, Call, Raise
 
+        Scenario: 
+        First player bets 200
+        Second player goes calls
+        Third Player goes all in for 399 chips
+        The action isn't reopened because it isn't at least the minimum bet size
+        so first player can: Fold, or call
+        second player can: Fold, or call
+        """
+        print("It's your turn {}.".format(self.name))
+        print("What will you do?")
+        if self.cur_bet == cur_bet:
+            #Should only be called when it is preflop and the action gets back to the big blind and no one's raised
+            print("Check or Raise")
+        elif self.stack_size - cur_bet < 0:
+            print("Fold or All in")
+        elif cur_bet == 0:
+            print("Check or Bet")
+        #elif not action_is_open:
+        #    print("Fold or Call")
+        else:
+            print("Fold, call or raise")
+        import texas_holdem
+        action = input() 
+        print(action)
+        if action == "Fold":
+            bet = 0
+        elif action == "Bet":
+            print("{} chips".format(str(self.stack_size)))
+            bet = int(input("How much do you want to bet?"))
+        elif action == "Raise":
+            print("{} chips".format(str(self.stack_size)))
+            bet = int(input("How much do you want to bet?"))
+        elif action == "Call":
+            bet = 0
+        elif action == "Check":
+            bet = 0 
+        elif action == "All in":
+            bet = self.stack_size
+        else: 
+            raise ValueError("invalid Action")
+
+        return (action, bet)
+
+    def get_cur_bet(self):
+        return self.cur_bet
+
+    def add_to_stack(self, chips):
+        self.stack_size += chips
+
+    def win_hand(self, chips):
+        self.add_to_stack(chips)
+
+    def subtract_from_stack(self, chips):
+        chips = (chips - self.cur_bet)
+        if chips > self.stack_size:
+            raise ValueError("Not Enough chips, did you mean to go all in instead?")
+        self.stack_size -= chips 
+        self.cur_bet += chips
+
+    def display_player(self):
+        print("name: {}".format(self.name))
+        print("chips: {}".format(self.stack_size))
+        print("cur_bet: {}".format(self.cur_bet))
--- src/texas_holdem.py
+++ src/texas_holdem.py
@@ -4,6 +4,7 @@ from cards import Card, Suit, Deck
 
 from functools import reduce
 from player import Player
+from enum import Enum
 import sys
 import pickle
 
@@ -87,7 +88,92 @@ class TexasHandEvaluator():
         # If it's greater than one it can't be a flush
         return suit_check == 1
 
+class TexasHoldemActions(Enum):
+    """
+    Functionality pertaining to the various actions a player can take.
+    Mainly: check, bet, raise, and fold.
+    """
+    CHECK = 0
+    BET = 1
+    CALL = 2
+    RAISE = 3
+    FOLD = 4
+    ALLIN=5
 
+class TexasHoldemState():
+    """Class responsible for maintaining the state of the hand."""
+    def __init__(self):
+        self.deck = Deck()
+        self.deck.shuffle()
+        self.board = []
+        self.button = 0
+        
+        self.big_blind = 2
+        self.small_blind = 1
+        self.pot = self.big_blind + self.small_blind
+        self.cur_bet = 2
+        # Used to determine when to move to the next street
+        # If play goes around to the last aggressor, Move to next street
+        # Special note may be preflop Big blind
+        self.last_aggressor = Player("Not A Person")
+        self.counter = 0
+        # check/bet/raise/fold = 0/1/2/3
+        self.last_action = 0
+        self.minimum_bet_size = 2
+        self.is_anti = False
+        self.anti = 0
+
+    def flop(self):
+        """Deals three cards on the flop"""
+        for i in range(3):
+           self.board.append(self.deck.deal_card())
+        self.last_action = 0
+        self.cur_bet = 0
+        self.clean_state()
+
+    def turn(self):
+        """Deals one card on the turn"""
+        self.board.append(self.deck.deal_card())
+        self.clean_state()
+
+    def river(self):
+        """Deals one card on the river"""
+        self.board.append(self.deck.deal_card())
+        self.clean_state()
+
+    def reinit(self, num_of_players):
+        """Reinitialize state for the beginning of a new hand"""
+        self.board = []
+        self.deck.shuffle()
+        self.button = (self.button + 1) % num_of_players
+        self.pot = self.big_blind + self.small_blind
+        self.minimum_bet_size = self.big_blind
+        self.cur_bet = self.big_blind
+        self.last_aggressor = Player("Not a Player")
+        self.counter = 0
+
+    def clean_state(self):
+        """Cleans up the state after action closes on a street"""
+        self.last_action = 0
+        self.cur_bet = 0
+        self.minimum_bet_size = 0
+        self.last_aggressor = Player("Not a PLayer")
+        self.counter = 0
+
+    def display_board(self):
+        """Print the cards on the table."""
+        print([str(c) for c in self.board])
+
+    def display_state(self):
+        print("current pot size is {}".format(self.pot))
+        print("current minimum bet is {}".format(self.minimum_bet_size))
+        self.display_board()
+
+class EOHError(EOFError):
+    """Error used to signal that a hand's done"""
+    def __init__(self,arg):
+        self.strerror=arg
+        self.args = {arg}
 
 class TexasHoldemGame():
     """Game Implementations of Texas Hold'em."""
@@ -95,33 +181,197 @@ class TexasHoldemGame():
     def __init__(self, *players):
         """Crete the Evaluator, Deck, and Players."""
         self.evaluator = TexasHandEvaluator()
-        self.deck = Deck()
-        self.players = players
+        self.players = list(players)
+        self.cur_players = list(players)
+        self.state = TexasHoldemState()
 
     def start_hand(self):
-        """Clear the board, shuffle the deck, and deal the cards."""
-        self.board = []
-        self.deck.shuffle()
-        self.deal_hole_cards()
-    
+        """Clear the board, shuffle the deck, and begin the hand"""
+        self.state.reinit(len(self.players))
+        self.deal_hole_cards() 
+        
+        #Set up the blinds
+        self.small_blind = (self.state.button + 1) % len(self.players)
+        self.players[self.small_blind].subtract_from_stack(1)
+
+        self.big_blind = (self.state.button + 2) % len(self.players)  
+        self.players[self.big_blind].subtract_from_stack(2)
+
+        self.play_hand()
+
+    def play_hand(self):
+        try:
+            self.preflop_actions()
+            self.flop_actions()
+            self.turn_actions()
+            self.river_actions()
+            self.showdown()
+            self.end_hand()
+        except EOHError:
+            self.end_hand()
+
+    def get_player_actions(self):
+        """
+        Go through all the player actions.
+        Check/Bet/Raise/Fold
+        """
+        #TODO:Should start at small blind, but this works for now
+        #for player in self.cur_players:
+        #    player.display_player()
+        #    action, bet = player.get_action(self.state.cur_bet) 
+        #   
+        #    self.perform_player_action(player, action, bet)
+        
+        cur_player_index = self.small_blind
+        if self.state.board == []:
+            cur_player_index = (self.big_blind + 1) % len(self.cur_players)
+        initial_player_count = len(self.cur_players)
+
+        while len(self.cur_players) > 1:
+            player = self.cur_players[cur_player_index]
+            if player == self.state.last_aggressor or self.state.counter == initial_player_count:
+                break
+            # Handle Preflop 
+            # Checks to see if there've been more actions than players
+            # If there's a bet, it resets the counter
+            print("pot: {} chips".format(self.state.pot))
+            player.display_player()
+
+            action, bet = player.get_action(self.state.cur_bet) 
+           
+            self.perform_player_action(player, action, bet)
+            cur_player_index = (cur_player_index + 1) % len(self.cur_players)
+
+            self.state.counter += 1
+        if len(self.cur_players) == 1:
+            raise EOHError("End of Hand")
+        #Reset players for the next street
+        for player in self.cur_players:
+            player.cur_bet = 0
+
+    def preflop_actions(self):
+        print("Preflop")
+        self.state.display_state()
+        self.get_player_actions()
+
+    def flop_actions(self):
+        print("Flop")
+        self.state.flop()
+        self.state.display_state()
+        self.get_player_actions()
+
+
+    def turn_actions(self):
+        print("turn")
+        self.state.turn()
+        self.state.display_state()
+        self.get_player_actions()
+
+    def river_actions(self):
+        print("river")
+        self.state.river()
+        self.state.display_state()
+        self.get_player_actions()
+
     def deal_hole_cards(self):
         """Deals cards to every player."""
         for player in self.players:
-            hand = TexasHoldemHand(self.deck.deal_card(),self.deck.deal_card())
+            #TODO: create a function to deal texas holdem hands in state
+            hand = TexasHoldemHand(self.state.deck.deal_card(),self.state.deck.deal_card())
             player.set_hand(hand)
 
-    def flop(self):
-        """Deals three cards on the flop"""
-        for i in range(3):
-           self.board.append(self.deck.deal_card())
 
-    def turn(self):
-        """Deals one card on the turn"""
-        self.board.append(self.deck.deal_card())
-   
-    def river(self):
-        """Deals one card on the river"""
-        self.board.append(self.deck.deal_card())
+
+    def perform_player_action(self, player, action, bet):
+        """
+        Handle actions which will affect the state.
+        May throw value errors if a bet size is too small.
+        Seems like raising and betting have the same affect,
+        but I'm going to keep them separate just in case it needs to be changed.
+        """
+        
+        if action == "Check":
+            self.handle_check(player, bet)
+        elif action == "Bet":
+            self.handle_bet(player, bet)
+        elif action =="Call":
+            self.handle_call(player, bet)
+        elif action =="Raise":
+            self.handle_bet(player, bet)
+        elif action == "Fold":
+            self.handle_fold(player, bet)
+        elif action == "All in":
+            self.handle_all_in(player, bet)
+        else:
+            raise ValueError("Invalid action: {}".format(str(action)))
+
+
+    def handle_call(self, player, bet):
+        """
+        Handles the Call action.
+        There has to be a current bet,
+        otherwise raises a ValueError
+        """
+        if self.state.cur_bet == 0:
+            raise ValueError("You can't call nothing")
+        #A player might have been raised and already have chips in the pot
+        self.state.pot += (self.state.cur_bet - player.get_cur_bet())
+        player.subtract_from_stack(self.state.cur_bet)
+
+    def handle_check(self, player, bet):
+        """
+        Handles the check action.
+        If the last action performed was, checking isn't allowed. 
+        Raise an error.
+        """
+        if self.state.cur_bet != player.get_cur_bet():
+            #Unless you're the bigblind and it checks around to you
+            raise ValueError("You cannot check when there's been a bet")
+       
+    def handle_bet(self, player, bet):
+        """
+        Handles a bets effect on the board state.
+        Raises an error dependent on betsize
+        """
+        if (bet - self.state.cur_bet) < self.state.minimum_bet_size:
+            raise ValueError("Bet is smaller than minimum bet size.")
+        # Example: min bet starts at 2 preflop. I open to 6. min bet size should be 4
+        # I get reraised to 10. Min bet size should still be 4. 10 - 6 = 4
+        self.state.cur_bet = bet
+        self.state.pot += (bet - player.get_cur_bet())
+        self.state.minimum_bet_size = (self.state.cur_bet - self.state.minimum_bet_size)
+        self.state.last_aggressor = player 
+        player.subtract_from_stack(bet)
+        self.state.counter = 0
+        
+    def handle_raise(self, bet):
+        """
+        Same as handle_bet.
+        Blank for now, use handle_bet instead
+        """
+        pass
+
+    def handle_fold(self, player, bet):
+        """
+        Handles the fold action
+        Ask if they really want to fold when there's no reason to
+        """
+        if self.state.cur_bet == 0:
+            raise ValueError("You know you don't have to fold right?")
+        self.cur_players.remove(player)
+
+    def handle_all_in(self, bet):
+        """
+        Handles all in bets
+        Sometimes a player will have less than the minimum bet size, 
+        but they still want to bet/raise. Handle bet will throw an error
+        so this is needed for when it's the rest of a players stack
+        This shouldn't reopen action, and should only be used when
+        the player has less than min bet size.
+        """
+        self.last_aggressor = player 
+        self.pot += bet
+        self.cur_bet += bet
 
     def showdown(self):
         """
@@ -132,9 +382,10 @@ class TexasHoldemGame():
         # Pick the winner
         best_hand = -1
         winner_name = "Wrong"
+        winner = None
         for player in self.players:
             # combine the hand with the board
-            full_hand = self.board + player.get_hand().get_hole_cards()
+            full_hand = self.state.board + player.get_hand().get_hole_cards()
             player_best = -1
             # Convert to 5 card hands and get the best val
             for combo in combinations(full_hand, 5):
@@ -144,18 +395,31 @@ class TexasHoldemGame():
                     if rank > best_hand:
                         best_hand = rank
                         winner_name = player.get_name()
-            print(player.name + ": {}".format(player_best))
+                        winner = player
+        self.state.display_state()
+        print(player.get_hand())
+        print(player.name + ": {}".format(player_best))
         print(best_hand)
         print(winner_name)
+        return winner
+    
+    def end_hand(self):
+        """Cleanup required after the hand's over"""
+        winner = None
+        if len(self.cur_players) == 1:
+            winner = self.cur_players[0]
+        else:
+            # Doesn't currently handle ties, or side pots
+            winner = self.showdown()
+        print("{} has won the hand".format(winner.get_name()))
+        print("{} has won {} chips".format(winner.get_name(), self.state.pot))
+        winner.win_hand(self.state.pot)
 
     def display_players(self):
         """Print information about the players"""
         for player in self.players:
             print("{}: {}".format(player.name, player.get_hand()))
    
-    def display_board(self):
-        """Print the cards on the table."""
-        print([str(c) for c in self.board])
 
 if __name__ == "__main__":
     print("Starting game")
@@ -163,9 +427,3 @@ if __name__ == "__main__":
     player2 = Player("Morty")
     game = TexasHoldemGame(player1, player2)
     game.start_hand()
-    game.display_players()
-    game.flop()
-    game.turn()
-    game.river()
-    game.showdown()
-    game.display_board()
